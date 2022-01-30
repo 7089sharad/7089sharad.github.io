7264e78f7088b09f42292785cd8f346d0afd305b
7089sharad <goursharad145@gmail.com>
2022-01-30 23:37:30 +0530

    finally humne commit krdiya

---
+++ lab5/java.js
@@ -0,0 +1,13 @@
+function fun2(){
+    but2.style.backgroundColor="#8c96ab";  
+    but2.style.color="white";
+   but3.style.backgroundColor="white"; 
+    but3.style.color="#8b92a3";
+   
+  }
+  function fun3(){
+    but3.style.backgroundColor="#8c96ab";  
+    but3.style.color="white";
+   but2.style.backgroundColor="white"; 
+    but2.style.color="#8b92a3";
+     }
